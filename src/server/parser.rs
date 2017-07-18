81996134b5c97b33181032d434a172062e621b7e
Nisarg Patel <npatel33@gmu.edu>
2017-07-18 13:32:56 +0000

    Adds comments

--- src/server/parser.rs
+++ src/server/parser.rs
@@ -1,11 +1,24 @@
 use std::str;
 
 /// Implementation packet parser
-
+///
+/// # Methods
+///
+/// * `fn parse_packet()` : parses packet from buffer
 pub struct Parser;
 
 impl Parser {
 
+    /// Parse received packets
+    ///
+    /// # Arguments
+    ///
+    /// * `buf` : reference to packet buffer
+    ///
+    /// This function parses received packets from
+    /// client to interpret function codes. According
+    /// to proper function codes, corresponding action
+    /// is taken on GPIO
     pub fn parse_packet(&self, mut buf: &[u8])
     {
         /*
