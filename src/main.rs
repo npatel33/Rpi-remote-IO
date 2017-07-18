766073b681eb770cca9ba005e275728c54796fb9
npatel33 <npatel33@gmu.edu>
2017-07-17 20:57:52 -0400

    Runs server

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,7 @@ fn main() {
 
     let mut s = server::Server {
         address : "127.0.0.1:3333".to_string(),
-        max_conn : 100
     };
 
-    println!("Maximum connections : {}", s.get_max_conn());
+    s.start();
 }
